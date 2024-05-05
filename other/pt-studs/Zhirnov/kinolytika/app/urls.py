37980eaa18a24a65a445b52eac9819e176d3ceaf
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-05-05 21:17:52 +0300

    Added index page

---
+++ other/pt-studs/Zhirnov/kinolytika/app/urls.py
@@ -0,0 +1,4 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+urlpatterns = []
--- other/pt-studs/Zhirnov/kinolytika/kinolytika/urls.py
+++ other/pt-studs/Zhirnov/kinolytika/kinolytika/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
