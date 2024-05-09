aa31e69632e00cc43ae7bc023393dfc2a20c70ff
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-05-09 13:29:52 +0300

    Added: Login and Logup views

--- other/pt-studs/Zhirnov/kinolytika/kinolytika/settings.py
+++ other/pt-studs/Zhirnov/kinolytika/kinolytika/settings.py
@@ -57,18 +57,12 @@ DATABASES = {
     }
 }
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
