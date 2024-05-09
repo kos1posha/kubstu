5f92b3e10af10c0fb08947fa3219759bef10b132
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-05-09 20:00:28 +0300

    Added: Filmshows list view

--- other/pt-studs/Zhirnov/kinolytika/app/urls.py
+++ other/pt-studs/Zhirnov/kinolytika/app/urls.py
@@ -6,5 +6,6 @@ import app.views as views
 app_name = 'kino'
 urlpatterns = [
     path('entry/', views.LoginView.as_view(template_name='login_logup.html'), name='login'),
-    path('register/', views.RegisterView.as_view(), name='register')
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('films/', views.FilmsView.as_view(), name='films')
 ]
--- other/pt-studs/Zhirnov/kinolytika/app/views.py
+++ other/pt-studs/Zhirnov/kinolytika/app/views.py
@@ -2,7 +2,10 @@ from django.contrib.auth import authenticate, login
 import django.contrib.auth.forms as auth_forms
 import django.contrib.auth.views as auth_views
 from django.urls import reverse_lazy
-import django.views.generic as generic_view
+from django.utils import timezone
+import django.views.generic as generic_views
+
+from app.models import FilmShow
 
 
 class LoginView(auth_views.LoginView):
@@ -19,7 +22,7 @@ class LoginView(auth_views.LoginView):
         return context
 
 
-class RegisterView(generic_view.FormView):
+class RegisterView(generic_views.FormView):
     form_class = auth_forms.UserCreationForm
     success_url = reverse_lazy('index')
     http_method_names = ['post']
@@ -31,3 +34,16 @@ class RegisterView(generic_view.FormView):
         user = authenticate(username=username, password=password)
         login(self.request, user)
         return super().form_valid(form)
+
+
+class FilmsView(generic_views.ListView):
+    template_name = 'films.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+    def get_queryset(self):
+        active_films = sorted(FilmShow.objects.all().filter(datetime__gt=timezone.now()), key=lambda f: f.datetime)
+        return active_films
