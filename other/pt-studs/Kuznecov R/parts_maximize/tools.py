87c87a804d7c04d1a84a3bd2ae0901555ebd9f1e
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-05-14 16:31:04 +0300

    Added: Tools file

---
+++ other/pt-studs/Kuznecov R/parts_maximize/tools.py
@@ -0,0 +1,39 @@
+import re
+
+
+def num(x):
+    def tryfloat(x):
+        try:
+            return float(x)
+        except:
+            return x
+
+    def tryint(x):
+        try:
+            return int(x)
+        except:
+            return x
+
+    if isinstance(x, str):
+        if ',' in x:
+            return round(tryfloat(x.replace(',', '.')), 2)
+        else:
+            return tryint(x)
+
+    return x
+
+
+subs = {f'x{i}': f'x<sub>{i}</sub>' for i in range(1, 6)}
+replace = lambda match: subs[match.group(0)]
+
+
+def xi_to_subscripts(string):
+    return re.sub(r'x[1-5]', replace, string)
+
+
+def shift_next(lst):
+    return lst[-1:] + lst[:-1]
+
+
+def shift_prev(lst):
+    return lst[1:] + lst[:1]
