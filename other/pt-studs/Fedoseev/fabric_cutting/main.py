6ad497dc692a3512b77353c0fab2234c6c5bd19a
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-05-18 15:30:15 +0300

    Update: Set app style as Fusion

--- other/pt-studs/Fedoseev/fabric_cutting/main.py
+++ other/pt-studs/Fedoseev/fabric_cutting/main.py
@@ -7,6 +7,7 @@ from qt.controls.given import GivenControl
 
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv + ['-platform', 'windows:darkmode=1'])
+    app.setStyle('Fusion')
     control = GivenControl()
     control.show()
     status = app.exec()
