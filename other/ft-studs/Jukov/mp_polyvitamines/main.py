b34465bce6121fb3b77cf1587a2ec120f110cbab
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-05-25 23:32:13 +0300

    Updated: Changed app theme to light

--- other/ft-studs/Jukov/mp_polyvitamines/main.py
+++ other/ft-studs/Jukov/mp_polyvitamines/main.py
@@ -6,7 +6,7 @@ from qt.control import VitaminsLpSolverControl
 
 
 if __name__ == '__main__':
-    app = qtw.QApplication(sys.argv)
+    app = qtw.QApplication(sys.argv + ['-platform', 'windows:darkmode=1'])
     app.setStyle('Fusion')
     control = VitaminsLpSolverControl(show=True)
     status = app.exec()
