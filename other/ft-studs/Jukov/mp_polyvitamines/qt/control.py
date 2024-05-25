536ba08359877f46cf95b7afc145b04786402e3c
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-05-26 00:22:47 +0300

    Updated: Made output more prettier, added buttons for clear output, showed task and put default values

--- other/ft-studs/Jukov/mp_polyvitamines/qt/control.py
+++ other/ft-studs/Jukov/mp_polyvitamines/qt/control.py
@@ -1,3 +1,5 @@
+import os
+
 from PySide6 import QtGui as qtg, QtWidgets as qtw
 
 import lpp
@@ -26,10 +28,14 @@ class VitaminsLpSolverControl(Ui_VitaminsLpSolverWindow, qtw.QWidget):
         super().setupUi(self)
         self.setFixedSize(self.width(), self.height())
         self.tw_model.setItemDelegate(NumericDelegate(self.tw_model))
-        self.tw_model.horizontalHeader().setSectionResizeMode(qtw.QHeaderView.ResizeMode.Fixed)
-        self.tw_model.verticalHeader().setSectionResizeMode(qtw.QHeaderView.ResizeMode.Fixed)
+        for tw in [self.tw_model, self.tw_point]:
+            tw.horizontalHeader().setSectionResizeMode(qtw.QHeaderView.ResizeMode.Stretch)
+            tw.verticalHeader().setSectionResizeMode(qtw.QHeaderView.ResizeMode.Stretch)
 
     def connect_ui(self):
+        self.pb_clear.clicked.connect(self.clear_output)
+        self.pb_task.clicked.connect(self.show_task)
+        self.pb_defaults.clicked.connect(self.defaults)
         self.pb_solve.clicked.connect(self.test)
 
     def get_tw_row(self, row):
@@ -52,16 +58,42 @@ class VitaminsLpSolverControl(Ui_VitaminsLpSolverWindow, qtw.QWidget):
         problem = lpp.vit_solve(a, c, b6, p)
         self.print_output(problem)
 
+    def clear_output(self):
+        self.l_extr.setText(f'Стоимость курса: н/д')
+        self.l_a.setText(f'Витамин A: н/д')
+        self.l_c.setText(f'Витамин C: н/д')
+        self.l_b6.setText(f'Витамин B₆: н/д')
+        for col in range(7):
+            self.tw_point.item(0, col).setText('н/д')
+
+    def defaults(self):
+        defaults = [
+            [5, 0, 2, 0, 3, 1, 2, 100],
+            [3, 1, 5, 0, 2, 0, 1, 80],
+            [1, 0, 3, 1, 2, 0, 6, 120],
+            [4, 1, 5, 6, 3.5, 7, 4]
+        ]
+        for row, d_row in enumerate(defaults):
+            for col, d in enumerate(d_row):
+                self.tw_model.item(row, col).setText(str(d))
+
+    def show_task(self):
+        pixmap = qtg.QPixmap(os.path.abspath(r'task.png'))
+        label = qtw.QLabel()
+        label.setPixmap(pixmap)
+        layout = qtw.QHBoxLayout()
+        layout.addWidget(label)
+        dialog = qtw.QDialog()
+        dialog.setLayout(layout)
+        dialog.setWindowTitle('Задание')
+        dialog.setStyleSheet('background:white')
+        dialog.exec()
+
     def print_output(self, problem):
-        self.te_output.clear()
-        self.te_output.insertPlainText(f'Статус: {"Оптимальный" if problem.status == 1 else "Нет решения"}\nРезультат: {problem.objective.value()}\nЗначения переменных:')
-        cursor = self.te_output.textCursor()
-        cursor.insertTable(2, 7)
-        for c in ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7']:
-            cursor.insertText(f'  {c}  ')
-            cursor.movePosition(qtg.QTextCursor.MoveOperation.NextCell)
-        for var in problem.variables():
-            cursor.insertText(f'  {int(var.value()):>2}  ')
-            cursor.movePosition(qtg.QTextCursor.MoveOperation.NextCell)
-        constraints = '\n'.join(f'{name:<2} = {int(constraint.value() - constraint.constant)} ({int(constraint.value())})' for name, constraint in problem.constraints.items())
-        self.te_output.insertPlainText(f'Ограничения:\n{constraints}')
+        self.l_extr.setText(f'Стоимость курса: {problem.objective.value()} тыс. руб.')
+        pv_values = [int(constr.value() - constr.constant) for constr in problem.constraints.values()]
+        self.l_a.setText(f'Витамин A: {pv_values[0]} гр.')
+        self.l_c.setText(f'Витамин C: {pv_values[1]} гр.')
+        self.l_b6.setText(f'Витамин B₆: {pv_values[2]} гр.')
+        for col, var in enumerate(problem.variables()):
+            self.tw_point.item(0, col).setText(str(int(var.value())))
