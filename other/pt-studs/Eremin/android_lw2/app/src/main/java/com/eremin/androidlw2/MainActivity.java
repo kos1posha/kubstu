d8f7ed1ee929afdbd50fcd2e494d537a73de3305
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-05-27 15:25:43 +0300

    Added: Action bar

--- other/pt-studs/Eremin/android_lw2/app/src/main/java/com/eremin/androidlw2/MainActivity.java
+++ other/pt-studs/Eremin/android_lw2/app/src/main/java/com/eremin/androidlw2/MainActivity.java
@@ -2,15 +2,21 @@ package com.eremin.androidlw2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.eremin.androidlw2.databinding.ActivityMainBinding;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
     private View view;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         binding = null;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_molecular_calculator) {
+            goToMolecularCountActivity(view);
+        } else if (id == R.id.action_about) {
+            Toast toast = Toast.makeText(this, "Да нечего рассказывать...", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     protected void setupListeners() {
