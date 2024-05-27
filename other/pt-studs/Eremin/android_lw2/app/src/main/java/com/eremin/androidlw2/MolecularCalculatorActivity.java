ca123d7433eca705d821877bd67b11b6e06ad7ff
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-05-27 13:07:34 +0300

    Added: The toast when input is empty

--- other/pt-studs/Eremin/android_lw2/app/src/main/java/com/eremin/androidlw2/MolecularCalculatorActivity.java
+++ other/pt-studs/Eremin/android_lw2/app/src/main/java/com/eremin/androidlw2/MolecularCalculatorActivity.java
@@ -1,22 +1,28 @@
 package com.eremin.androidlw2;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.eremin.androidlw2.databinding.ActivityMolecularCalculatorBinding;
+import com.eremin.androidlw2.databinding.EmptyInputToastBinding;
 
 import java.util.Locale;
 
 public class MolecularCalculatorActivity extends AppCompatActivity {
     private View view;
+    private LayoutInflater layoutInflater;
     private ActivityMolecularCalculatorBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMolecularCalculatorBinding.inflate(getLayoutInflater());
+        layoutInflater = getLayoutInflater();
+        binding = ActivityMolecularCalculatorBinding.inflate(layoutInflater);
         view = binding.getRoot();
         setContentView(view);
         setupListeners();
@@ -33,12 +39,25 @@ public class MolecularCalculatorActivity extends AppCompatActivity {
     }
 
     protected void calculate(View view) {
-        int quartsCount = Integer.parseInt(binding.etWaterQuartsCount.getText().toString());
+        String quartsString = binding.etWaterQuartsCount.getText().toString();
+        if (quartsString.isEmpty()) {
+            showEmptyInputToast();
+            return;
+        }
+        int quartsCount = Integer.parseInt(quartsString);
         double molecularCount = calculateMolecularCountInQuarts(quartsCount);
         binding.twMolecularCount.setText(String.format(Locale.ROOT, "%.0f", molecularCount));
     }
 
     protected double calculateMolecularCountInQuarts(int quartsCount) {
         return quartsCount * 950 / 3e-23;
+    }
+
+    protected void showEmptyInputToast() {
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.TOP, 0, 40);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(EmptyInputToastBinding.inflate(layoutInflater).getRoot());
+        toast.show();
     }
 }
