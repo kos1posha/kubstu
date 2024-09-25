b8aebe47a73c3162ff6f0ae68d5f98676b2f51e0
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-09-25 20:46:02 +0300

    Refactored: Turned main into a function

--- other/pt-studs/Eremin/ai/perceptron/main.py
+++ other/pt-studs/Eremin/ai/perceptron/main.py
@@ -4,9 +4,14 @@ from PySide6 import QtWidgets as qtw
 
 from qt import PerceptronControl
 
-if __name__ == '__main__':
+
+def main() -> None:
     app = qtw.QApplication(sys.argv)
     control = PerceptronControl()
     control.show()
     status = app.exec()
     sys.exit(status)
+
+
+if __name__ == '__main__':
+    main()
