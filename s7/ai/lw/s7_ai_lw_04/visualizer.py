a775596ecf24547948c88d2af64f1afe75a000a4
Альнаур Глоов <alnaur-gloov@mail.ru>
2024-10-20 17:51:13 +0300

    Updated: Used colormesh to display function values ​​on a plot

--- s7/ai/lw/s7_ai_lw_04/visualizer.py
+++ s7/ai/lw/s7_ai_lw_04/visualizer.py
@@ -1,27 +1,35 @@
+import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import mplcursors
 
 from genetic_algorithm import GeneticAlgorithmIterator
+
+
+cmap = cm.get_cmap('Reds_r')
 
 
 class GeneticAlgorithmPlotVisualizer:
     def __init__(self, genetic_iterator: GeneticAlgorithmIterator) -> None:
         super().__init__()
         self.i = genetic_iterator
+        self.X, self.Y = np.meshgrid(np.linspace(*self.i.bounds['x'], 25), np.linspace(*self.i.bounds['y'], 25))
+        self.C = self.i._np_func(self.X, self.Y)
         self.func_pretty = sp.pretty(sp.Eq(sp.Symbol('f(x, y)'), genetic_iterator.func))
 
     def visualize_population(self) -> None:
         plt.plot()
         xs, ys = zip(*self.i.population)
-        scatter = plt.scatter(xs, ys, color='black', s=10)
+        plt.pcolormesh(self.X, self.Y, self.C, cmap=cmap)
+        scatter = plt.scatter(xs, ys, color='black', s=5)
 
         plt.xlabel('x')
         plt.ylabel('y')
         plt.xlim(self.i.bounds['x'])
         plt.ylim(self.i.bounds['y'])
         plt.title(self.func_pretty)
-        plt.get_current_fig_manager().set_window_title(f'Популяция ({len(self.i.population)} хром.)')
+        plt.get_current_fig_manager().set_window_title(f'Популяция ({self.i.population_size} хром.)')
 
         cursor = mplcursors.cursor(scatter, hover=True)
 
@@ -29,6 +37,7 @@ class GeneticAlgorithmPlotVisualizer:
         def _(sel) -> None:
             index = sel.index
             ch, ht = self.i.population[index], self.i.health[index]
-            sel.annotation.set(text=f'Хромосома ({ch[0]:.2f}, {ch[1]:.2f})\nЗдоровье: {ht:.2f}', backgroundcolor='lightgray', alpha=1)
+            sel.annotation.set_text(f'Хромосома ({ch[0]:.2f}, {ch[1]:.2f})\nЗдоровье: {ht:.2f}')
+            sel.annotation.get_bbox_patch().set(fc='lightgray', alpha=1)
 
         plt.show()
